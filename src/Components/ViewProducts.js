bb5a989e870f620bfc186c19b43887d68e0f2763
WatchOut-RPG <sourav060606@gmail.com>
2025-02-13 18:07:21 +0530

    loading added

--- src/Components/ViewProducts.js
+++ src/Components/ViewProducts.js
@@ -3,40 +3,49 @@ import ViewProductsChild from './ViewProductsChild';
 import Navbar from './Navbar';
 import { useEffect, useState } from 'react';
 import BottomPage from './BottomPage';
-
+import loading from '../assets/loading.gif';
 
 export default function ViewProducts() {
   const [viPro, setViPro] = useState([]);
+  const [isLoading, setIsLoading] = useState(true); 
 
   const recieveProducts = () => {    
     axios.get('http://localhost:4001/view')
-    .then((res) => res.data)
-    .then((finalRes) => {      
-      setViPro(finalRes);
-    }).catch(error => console.error('Error:', error));
-  }
+      .then((res) => res.data)
+      .then((finalRes) => {      
+        setViPro(finalRes);
+        setIsLoading(false); 
+      })
+      .catch(error => {
+        console.error('Error:', error);
+        setIsLoading(false);
+      });
+  };
 
   useEffect(() => {
     recieveProducts();
   }, []);
 
+  
   const pView = viPro.map((product, index) => {
-    return(
-      <ViewProductsChild key={index} viewData={product}/>
-    );
+    return <ViewProductsChild key={index} viewData={product} />;
   });
-
 
   return (
     <>
-      <Navbar/>      
+      <Navbar />
       <div className='mt-[100px]'>
-        <div className='grid grid-cols-6 gap-6 py-4 max-sm:grid max-sm:grid-cols-2'>
-          {pView}
-        </div>         
+        {isLoading ? (
+          <div className='flex justify-center'>
+            <img src={loading} alt="Loading..." />
+          </div>
+        ) : viPro.length > 0 && (
+          <div className='grid grid-cols-6 gap-6 py-4 max-sm:grid-cols-2'>
+            {pView}
+          </div>
+        )}
       </div>
-      <BottomPage/>       
+      <BottomPage />
     </>
-  )
+  );
 }
-
